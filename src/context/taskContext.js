c4a13213dddafd30fa8c731f4335cd3471e5a37a
Regan Tam <regantam92@gmail.com>
2024-09-17 09:54:30 +1000

    Refactor

--- src/context/taskContext.js
+++ src/context/taskContext.js
@@ -2,76 +2,11 @@ import React, { createContext, useState, useEffect, useContext } from "react";
 import { LoadUpLocalStorage, saveToLocalStorage } from "../utils/StorageUtils";
 import { BinContext } from "./binContext";
 import { convertDueByDays, convertDueByHours } from "../utils/dateUtils";
-import { v4 as uuidv4 } from "uuid";
-import {
-  setInitialDate,
-  getDateDaysFromToday,
-  getDateDDMMYYYY,
-} from "../utils/dateUtils";
-import { useLocalStorage } from "../hooks/useLocalStorage";
-export const TaskContext = createContext();
 
-const initialTasks = [
-  {
-    key: uuidv4(),
-    title: "Complete Project Report",
-    description:
-      "Finish the final report for the project and submit it to the manager.",
-    dateDue: setInitialDate(getDateDaysFromToday(5)),
-    dateCreated: getDateDDMMYYYY(),
-    dateDeleted: "",
-    notified: { dueSoon: false, overDue: false },
-    status: "In Progress",
-    priority: "4",
-  },
-  {
-    key: uuidv4(),
-    title: "Schedule Doctor's Appointment",
-    description: "Call the clinic to schedule a routine check-up appointment.",
-    dateDue: setInitialDate(getDateDaysFromToday(1)),
-    dateCreated: getDateDDMMYYYY(),
-    dateDeleted: "",
-    status: "Pending",
-    priority: "1",
-    notified: { dueSoon: false, overDue: false },
-  },
-  {
-    key: uuidv4(),
-    title: "Team Meeting",
-    description:
-      "Join the weekly team meeting to discuss project updates and plans.",
-    dateDue: setInitialDate(getDateDaysFromToday(2)),
-    dateCreated: getDateDDMMYYYY(),
-    dateDeleted: "",
-    status: "Pending",
-    priority: "3",
-    notified: { dueSoon: false, overDue: false },
-  },
-  {
-    key: uuidv4(),
-    title: "Buy Groceries",
-    description:
-      "Purchase milk, eggs, bread, and fruit from the grocery store.",
-    dateDue: setInitialDate(getDateDaysFromToday(7)),
-    dateCreated: getDateDDMMYYYY(),
-    dateDeleted: "",
-    status: "Pending",
-    priority: "2",
-    notified: { dueSoon: false, overDue: false },
-  },
-  {
-    key: uuidv4(),
-    title: "Read a Book",
-    description:
-      "Read at least 50 pages of 'The Great Gatsby' by F. Scott Fitzgerald.",
-    dateDue: setInitialDate(getDateDaysFromToday(10)),
-    dateCreated: getDateDDMMYYYY(),
-    dateDeleted: "",
-    status: "Pending",
-    priority: "1",
-    notified: { dueSoon: false, overDue: false },
-  },
-];
+import { useLocalStorage } from "../hooks/useLocalStorage";
+import { initialTasks } from "../data/tasksData";
+
+export const TaskContext = createContext();
 
 export const TaskProvider = ({ children }) => {
   const [taskItems, setTaskItems] = useLocalStorage(
---
+++ src/data/tasksData.js
@@ -0,0 +1,68 @@
+import { v4 as uuidv4 } from "uuid";
+import {
+  setInitialDate,
+  getDateDaysFromToday,
+  getDateDDMMYYYY,
+} from "../utils/dateUtils";
+
+export const initialTasks = [
+  {
+    key: uuidv4(),
+    title: "Complete Project Report",
+    description:
+      "Finish the final report for the project and submit it to the manager.",
+    dateDue: setInitialDate(getDateDaysFromToday(5)),
+    dateCreated: getDateDDMMYYYY(),
+    dateDeleted: "",
+    notified: { dueSoon: false, overDue: false },
+    status: "In Progress",
+    priority: "4",
+  },
+  {
+    key: uuidv4(),
+    title: "Schedule Doctor's Appointment",
+    description: "Call the clinic to schedule a routine check-up appointment.",
+    dateDue: setInitialDate(getDateDaysFromToday(1)),
+    dateCreated: getDateDDMMYYYY(),
+    dateDeleted: "",
+    status: "Pending",
+    priority: "1",
+    notified: { dueSoon: false, overDue: false },
+  },
+  {
+    key: uuidv4(),
+    title: "Team Meeting",
+    description:
+      "Join the weekly team meeting to discuss project updates and plans.",
+    dateDue: setInitialDate(getDateDaysFromToday(2)),
+    dateCreated: getDateDDMMYYYY(),
+    dateDeleted: "",
+    status: "Pending",
+    priority: "3",
+    notified: { dueSoon: false, overDue: false },
+  },
+  {
+    key: uuidv4(),
+    title: "Buy Groceries",
+    description:
+      "Purchase milk, eggs, bread, and fruit from the grocery store.",
+    dateDue: setInitialDate(getDateDaysFromToday(7)),
+    dateCreated: getDateDDMMYYYY(),
+    dateDeleted: "",
+    status: "Pending",
+    priority: "2",
+    notified: { dueSoon: false, overDue: false },
+  },
+  {
+    key: uuidv4(),
+    title: "Read a Book",
+    description:
+      "Read at least 50 pages of 'The Great Gatsby' by F. Scott Fitzgerald.",
+    dateDue: setInitialDate(getDateDaysFromToday(10)),
+    dateCreated: getDateDDMMYYYY(),
+    dateDeleted: "",
+    status: "Pending",
+    priority: "1",
+    notified: { dueSoon: false, overDue: false },
+  },
+];
